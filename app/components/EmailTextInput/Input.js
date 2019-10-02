bd9fa6adc289ed9d746140affad09a979563275b
gpadua <gpadua@iouu.com.br>
2019-10-02 13:22:04 -0300

    Estrutura do projeto

---
+++ app/components/EmailTextInput/Input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+    Text,
+    TextInput,
+} from 'react-native';
+
+const InputEmail: () => React$Node = () => {
+    return (
+        <View>
+            <TextInput></TextInput>
+        </View>
+    )
+}
+
+export default InputEmail;
+

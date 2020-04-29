87a6491d6842a29039af699dcdfe1d01762adaa5
Leo Lana <llana@iouu.com.br>
2020-04-29 00:37:09 -0300

    feat: prettier in all files

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,18 @@
-const { getDefaultConfig } = require("metro-config");
+import { getDefaultConfig } from 'metro-config';
 
-module.exports = (async () => {
+export default (async () => {
   const {
-    resolver: { sourceExts, assetExts }
+    resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
   return {
     transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
       experimentalImportSupport: false,
       inlineRequires: false,
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
   };
 })();

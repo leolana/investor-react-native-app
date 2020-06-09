89e2af5b5987327b23c104e448482db13b332554
glati <glati@iouu.com.br>
2020-06-09 10:31:56 -0300

    fix: urls

--- src/services/urls/index.ts
+++ src/services/urls/index.ts
@@ -1,11 +1,11 @@
 const PROD = {
-  BASE_URL_GO: 'http://192.168.56.209:9090/',
+  BASE_URL_GO: 'https://api.iouu.com.br/',
   BASE_URL_NODE: 'https://server-app.iouu.com.br/',
   BASE_URL_HUB: 'https://hub-app.iouu.com.br/',
 };
 
 const TEST = {
-  BASE_URL_GO: 'http://192.168.0.17:9090/',
+  BASE_URL_GO: 'https://api-test.iouu.com.br/',
   BASE_URL_NODE: 'https://server-test.iouu.com.br/',
   BASE_URL_HUB: 'https://hub-test.iouu.com.br/',
 };
@@ -28,14 +28,17 @@ export const UrlRecuperarSenha = `${BASE_URL_NODE}api/v1/send-reset-password-ema
 
 export const UrlListaOportunidades = (pagina, scores) => `${BASE_URL_GO}api/v1/solicitacoes/listar/${pagina}/${scores}`;
 
-export const UrlSuitabilitySalvar = (usuarioId, passo) => `${BASE_URL_GO}api/v1/suitability/salvar/${usuarioId}/${passo}`;
+export const UrlSuitabilitySalvar = (usuarioId, passo) =>
+  `${BASE_URL_GO}api/v1/suitability/salvar/${usuarioId}/${passo}`;
 export const UrlSuitabilityPegar = (usuarioId) => `${BASE_URL_GO}api/v1/suitability/user/${usuarioId}`;
 export const UrlSuitabilityPegarPerfil = (suitabilityId) => `${BASE_URL_HUB}iouu/suitability/${suitabilityId}`;
 
 export const UrlCadastroUsuario = `${BASE_URL_GO}sign_up`;
 export const UrlCadastroInvestidor = `${BASE_URL_GO}api/v1/investidores`;
-export const UrlCadastroInvestidorAtualizar = (investidorId, passo) => `${BASE_URL_GO}api/v1/investidor/atualizar/${investidorId}/${passo}`;
-export const UrlCadastroInvestidorDocs = (investidorId, docTipo) =>`${BASE_URL_GO}api/v1/investidor/enviar/docs/b64/${investidorId}/${docTipo}`;
+export const UrlCadastroInvestidorAtualizar = (investidorId, passo) =>
+  `${BASE_URL_GO}api/v1/investidor/atualizar/${investidorId}/${passo}`;
+export const UrlCadastroInvestidorDocs = (investidorId, docTipo) =>
+  `${BASE_URL_GO}api/v1/investidor/enviar/docs/b64/${investidorId}/${docTipo}`;
 
 export const UrlLocalizacaoEstadosPegar = `${BASE_URL_NODE}api/v1/estados/`;
 export const UrlLocalizacaoCidadesPegar = (uf) => `${BASE_URL_NODE}api/v1/estados/${uf}/cidades`;
@@ -46,15 +49,18 @@ export const UrlBancoListaPegar = `${BASE_URL_HUB}iouu/bancos`;
 export const UrlCarteiraBancoPegar = (codigoBanco) => `${BASE_URL_NODE}api/v1/wallet/bank/${codigoBanco}`;
 export const UrlCarteiraPegar = `${BASE_URL_NODE}api/v1/investimentos/meus/money`;
 export const UrlCarteiraEnviarTransferencia = `${BASE_URL_NODE}api/v1/wallet/new`;
-export const UrlCarteiraEmprestimoPegar = (investidorId, page) => `${BASE_URL_NODE}api/v1/wallet/loans/${investidorId}/${page}`;
-export const UrlCarteiraComprovantePegar = (investidorId) => `${BASE_URL_NODE}api/v1/wallet/comprovante/${investidorId}`;
+export const UrlCarteiraEmprestimoPegar = (investidorId, page) =>
+  `${BASE_URL_NODE}api/v1/wallet/loans/${investidorId}/${page}`;
+export const UrlCarteiraComprovantePegar = (investidorId) =>
+  `${BASE_URL_NODE}api/v1/wallet/comprovante/${investidorId}`;
 
 export const UrlCarteiraExtratoPaginado = (pagina) => `${BASE_URL_GO}api/v1/wallet/extrato/pagina/${pagina}`;
 export const UrlCarteiraPegarDetalhesOperacao = (id) => `${BASE_URL_GO}api/v1/wallet/operacao/detalhe/${id}`;
 export const UrlCarteiraSaldo = `${BASE_URL_GO}api/v1/wallet/saldo`;
 
 export const UrlInfoInvLista = `${BASE_URL_NODE}api/v1/investimentos/meus`;
-export const UrlInfoInvSolicitacao = (solicitacaoId) => `${BASE_URL_NODE}api/v1/investimentos/meus/solicitacoes/${solicitacaoId}`;
+export const UrlInfoInvSolicitacao = (solicitacaoId) =>
+  `${BASE_URL_NODE}api/v1/investimentos/meus/solicitacoes/${solicitacaoId}`;
 
 export const UrlInvPegar = (email) => `${BASE_URL_GO}api/v1/investidor/email/${email}`;
 
@@ -66,16 +72,21 @@ export const UrlTomadorFatura = (id) => `${BASE_URL_NODE}api/v1/FaturaTomador/so
 
 export const UrlSolicitacaoPegar = (id) => `${BASE_URL_NODE}api/v1/solicitacoes/${id}`;
 export const UrlSolicitacaoReservaPegar = (reservaId) => `${BASE_URL_NODE}api/v1/reserva/solicitacao/${reservaId}`;
-export const UrlSolicitacaoReservaInvPegar = (solicitacaoId) => `${BASE_URL_NODE}api/v1/solicitacoes/${solicitacaoId}/reserva-investimento`;
-export const UrlSolicitacaoReservaInvCancelar = (solicitacaoId) => `${BASE_URL_NODE}api/v1/reserva-investimentos-CANCEL/${solicitacaoId}`;
-export const UrlSolicitacaoReservaInvCriar = (solicitacaoId) => `${BASE_URL_NODE}api/v1/solicitacoes/${solicitacaoId}/reserva-investimento`;
+export const UrlSolicitacaoReservaInvPegar = (solicitacaoId) =>
+  `${BASE_URL_NODE}api/v1/solicitacoes/${solicitacaoId}/reserva-investimento`;
+export const UrlSolicitacaoReservaInvCancelar = (solicitacaoId) =>
+  `${BASE_URL_NODE}api/v1/reserva-investimentos-CANCEL/${solicitacaoId}`;
+export const UrlSolicitacaoReservaInvCriar = (solicitacaoId) =>
+  `${BASE_URL_NODE}api/v1/solicitacoes/${solicitacaoId}/reserva-investimento`;
 
-export const UrlReservationCreate = (solicitacaoId) => `${BASE_URL_GO}api/v1/reserva/criar/solicitacao/${solicitacaoId}`;
+export const UrlReservationCreate = (solicitacaoId) =>
+  `${BASE_URL_GO}api/v1/reserva/criar/solicitacao/${solicitacaoId}`;
 export const UrlReservationDelete = (reservaId) => `${BASE_URL_GO}api/v1/reserva/cancelar/${reservaId}`;
 export const UrlReservationCreateBankSlip = (reservaId) => `${BASE_URL_GO}api/v1/gerarBoleto/${reservaId}`;
 
 export const UrlEstatisticasPegar = (usuarioId) => `${BASE_URL_GO}api/v1/GetEstatisticas/${usuarioId}`;
-export const UrlEstatisticasPlataformaPegar = (tipo) => `${BASE_URL_GO}api/v1/GetEstatisticasPlataformaInvestidores/${tipo}`;
+export const UrlEstatisticasPlataformaPegar = (tipo) =>
+  `${BASE_URL_GO}api/v1/GetEstatisticasPlataformaInvestidores/${tipo}`;
 
 export const UrlNotificacoesPegar = (usuarioId) => `${BASE_URL_NODE}api/v1/user/notifications/${usuarioId}`;
 export const UrlNotificacoesStatusAtualizar = `${BASE_URL_NODE}api/v1/notifications/status/update`;
@@ -87,15 +98,18 @@ export const UrlPerfilConfigNotificacaoSalvar = `${BASE_URL_NODE}api/v1/user/not
 export const UrlDocumentosPegar = (tipo, id) => `${BASE_URL_NODE}api/v1/documentos/${tipo}/${id}`;
 export const UrlDocumentosSolicitacaoPegar = (id) => `${BASE_URL_NODE}api/v1/doc/solicitacao/${id}`;
 
-export const UrlCCBAssinaturaAtualizar = (reservaId) => `${BASE_URL_GO}api/v1/out/atualizaCCbsInvestidoresAssinatura/${reservaId}`;
+export const UrlCCBAssinaturaAtualizar = (reservaId) =>
+  `${BASE_URL_GO}api/v1/out/atualizaCCbsInvestidoresAssinatura/${reservaId}`;
 
 export const UrlSenhaVerificar = `${BASE_URL_NODE}api/v1/emprestimos/verifica-senha`;
 
 export const UrlTaxasPegar = `${BASE_URL_HUB}calculadora/taxas`;
 
-export const UrlLinkCCBImprimir = (codigo) => `https://bmpdigital.moneyp.com.br/imprimirccb.aspx?code=${codigo}&copias=1`;
+export const UrlLinkCCBImprimir = (codigo) =>
+  `https://bmpdigital.moneyp.com.br/imprimirccb.aspx?code=${codigo}&copias=1`;
 
-export const UrlMapaEndereco = (endereco) => `https://www.google.com/maps/embed/v1/place?key=AIzaSyC9YU8ifbsf9Ta6ZmpsVwIJfcXqKj4t_M8&q=${endereco}`;
+export const UrlMapaEndereco = (endereco) =>
+  `https://www.google.com/maps/embed/v1/place?key=AIzaSyC9YU8ifbsf9Ta6ZmpsVwIJfcXqKj4t_M8&q=${endereco}`;
 
 export const UrlIouuFacebook = `https://www.facebook.com/IouuBrasil/`;
 export const UrlIouuInstagram = `https://www.instagram.com/iouubrasil/`;
